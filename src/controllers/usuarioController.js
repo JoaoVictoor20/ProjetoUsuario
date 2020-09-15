a6246319f2448ac6d17352428e5bcc5f36ff2730
João Victor Silva de Assis <70240580+JoaoVictoor20@users.noreply.github.com>
2020-09-15 20:24:38 -0300

    Update usuarioController.js

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,14 +1,14 @@
 // Define a utilização do model usuario e a dependência http-status
 const Usuario = require('../models/usuario');
 const status = require('http-status');
- 
+
 // Cria o método Insert, obtendo os dados da request
 exports.Insert = (req, res, next) => {
     const nome = req.body.nome;
     const salario = req.body.salario;
     const dataNascimento = req.body.dataNascimento;
     const ativo = req.body.ativo;
- 
+
     // Popula cada um dos campos do model com os campos recebido na request
     // Insert no BD
     Usuario.create({
@@ -26,5 +26,28 @@ exports.Insert = (req, res, next) => {
             }
         })
         //catch = registra o que queremos que aconteca quando a Promise falhar
+        .catch(error => next(error));
+};
+exports.SelectAll = (req, res, next) => {
+    Usuario.findAll()
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            }
+        })
+        .catch(error => next(error));
+}
+ 
+exports.SelectDetail = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
         .catch(error => next(error));
 };
